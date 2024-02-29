9fb73dad354ca1a106aaaf90142af8c11dd20298
Paul Gowder <paul.gowder@gmail.com>
2024-02-29 12:10:21 -0600

    ok we've made some progress now the issue is in the hexstring call

--- js/pdf_worker.js
+++ js/pdf_worker.js
@@ -1,21 +1,23 @@
 
 import { PDFDocument, PDFHexString, PDFName } from 'pdf-lib';
 
-self.onmessage = function(e) {
+
+
+self.onmessage = async function(e) {
    switch (e.data.mtype)
     {
 	case 'embed':
 	  console.log("in worker");
-	  var pdfDoc = PDFDocument.load(e.data.bytes);
+	  var pdfDoc = await PDFDocument.load(e.data.bytes)
 	  console.log("opened pdf file");
 	  self.postMessage({mtype: 'progress', message: 'PDF loaded successfully for embedding. Working ...'});
 	  pdfDoc['getInfoDict'].set(PDFName.of('citations'), PDFHexString.fromText(e.data.citations));
 	  pdfDoc['getInfoDict'].set(PDFName.of('citationsFilename'), PDFHexString.fromText(e.data.citesFileName));
-	  const pdfOut = pdfDoc.save();
+	  const pdfOut = await pdfDoc.save();
 	  self.postMessage({mtype: 'pdfout', bytes: pdfOut});
 	break;
 	case 'extract':
-	  var pdfDoc = PDFDocument.load(e.data.bytes);
+	  var pdfDoc = await PDFDocument.load(e.data.bytes);
           self.postMessage({mtype: 'progress', message: 'PDF loaded successfully for extraction. Working ...'});
 	  var citations = pdfDoc.getInfoDict().get(PDFName.of('citations'))?.decodeText();
 	  var citationsFileName = pdfDoc.getInfoDict().get(PDFName.of('citationsFilename'))?.decodeText();
